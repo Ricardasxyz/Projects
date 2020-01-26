e874b24e65492c467d12e9b829fd54e384977e2b
Ričardas Staniulis <Viproshal@gmail.com>
2020-01-26 21:50:24 +0200

    Add files via upload

---
+++ cat_project/precache-manifest.cb1cb95b25083f6199fcb4dccc7c2931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90272465749137a7cd3f75f2dde664fd",
+    "url": "/Projektai/cat/index.html"
+  },
+  {
+    "revision": "246548cd32b99de755d7",
+    "url": "/Projektai/cat/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "46ce9069c2576d1ea70b",
+    "url": "/Projektai/cat/static/css/main.93db481c.chunk.css"
+  },
+  {
+    "revision": "246548cd32b99de755d7",
+    "url": "/Projektai/cat/static/js/2.fb5b6c84.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Projektai/cat/static/js/2.fb5b6c84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46ce9069c2576d1ea70b",
+    "url": "/Projektai/cat/static/js/main.e6257e6a.chunk.js"
+  },
+  {
+    "revision": "282db1301a42001a5401",
+    "url": "/Projektai/cat/static/js/runtime-main.190c4f90.js"
+  }
+]);
