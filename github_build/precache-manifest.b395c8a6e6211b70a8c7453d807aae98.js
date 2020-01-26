db0c084ee982d17ec0de716978fb3104e8da8129
Ričardas Staniulis <Viproshal@gmail.com>
2020-01-26 21:49:51 +0200

    Add files via upload

---
+++ github_build/precache-manifest.b395c8a6e6211b70a8c7453d807aae98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35c82982e96d4d71c1f2602d207abe7",
+    "url": "/Projektai/cat/index.html"
+  },
+  {
+    "revision": "7b0d81b5f001967883de",
+    "url": "/Projektai/cat/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "1f6bd2fe6b708b851b47",
+    "url": "/Projektai/cat/static/css/main.b1f08855.chunk.css"
+  },
+  {
+    "revision": "7b0d81b5f001967883de",
+    "url": "/Projektai/cat/static/js/2.081570a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Projektai/cat/static/js/2.081570a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f6bd2fe6b708b851b47",
+    "url": "/Projektai/cat/static/js/main.a36ad644.chunk.js"
+  },
+  {
+    "revision": "d25e6277ae6b924807a5",
+    "url": "/Projektai/cat/static/js/runtime-main.7b116319.js"
+  },
+  {
+    "revision": "5cd9f30e245cbdcc11274951e439ad65",
+    "url": "/Projektai/cat/static/media/git.5cd9f30e.png"
+  }
+]);
